058de91c19bef8ca386abab959bc9ec9bac1d193
Wenting Yan <wenting.yan@rea-group.com>
2017-02-23 21:23:38 +0800

    Add docker-compose

--- src/main/resources/db.migration/v1__InitTable.sql
+++ src/main/resources/db.migration/v1__InitTable.sql
@@ -7,3 +7,6 @@ CREATE TABLE item (
    text VARCHAR(128) NOT NULL,
    done Boolean NOT NULL DEFAULT FALSE
 );
+
+INSERT INTO item (text, done) VALUES ('this is first default item', true);
+INSERT INTO item (text) VALUES ('this is second default item');
--- src/main/resources/db.migration/v2__PopulateTestData.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO item (text, done) VALUES ('this is first default item', true);
-INSERT INTO item (text) VALUES ('this is second default item');
